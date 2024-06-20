54aceb17dba2a3086b22b0bf4b2037cea7db5cfd
Ch40s1 <114247747+Ch40s1@users.noreply.github.com>
2024-06-19 22:44:42 -0500

    added base files

--- main.js
+++ main.js
@@ -1,2 +1,4 @@
 console.log("hello world");
-
+// begin with a console.log statement to ensure that the file is being read correctly
+// get h1 tag and change content to "Hello World"
+document.querySelector("h1").innerHTML = "Hello World!!!!";
