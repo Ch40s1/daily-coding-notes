8205826f8be35cc88513f19f8267b1402e2c0280
Ch40s1 <114247747+Ch40s1@users.noreply.github.com>
2024-06-24 20:08:25 -0500

    added new topics with basic code

--- main.js
+++ main.js
@@ -1,3 +1,14 @@
 // get some data from the database
-var data = db.getData();
-// do something with the data
+const dsa_butns = {
+  "arrays": document.getElementById("arrays"),
+  "hashing": document.getElementById("hashing"),
+  "linked_lists": document.getElementById("linked_lists"),
+  "stacks": document.getElementById("stacks"),
+  "queues": document.getElementById("queues"),
+}
+
+dsa_butns.arrays.addEventListener("click", () => {
+  const para = document.createElement("p");
+  para.textContent = "You clicked arrays";
+  dsa_butns.arrays.appendChild(para);
+});
